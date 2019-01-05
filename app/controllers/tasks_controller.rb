8d5c7c6e96da52be1df7d630c500c938ef1d2b2a
soramame <walkworthyofthelord0128@gmail.com>
2019-01-05 09:38:16 +0000

    delete page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -24,7 +24,7 @@ class TasksController < ApplicationController
       flash[:success] = 'Task が正常に投稿されました'
       redirect_to @task
     else
-      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
+      @tasks = current_user.tasks.order('created_at DESC')
       flash.now[:danger] = 'Task が投稿されませんでした'
       render :new
     end
