f6967cc687f3f4185b2645e7cff22d501f53314f
soramame <walkworthyofthelord0128@gmail.com>
2019-01-05 04:37:02 +0000

    current

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :correct_user
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
     if logged_in?
@@ -51,7 +51,6 @@ class TasksController < ApplicationController
   end
 
 
-
 private
 
   def set_task
@@ -62,7 +61,7 @@ private
   def task_params
     params.require(:task).permit(:content, :status)
   end
-  
+
   def correct_user
     @task = current_user.tasks.find_by(id: params[:id])
     unless @task
